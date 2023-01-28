d2e3ced6a1f088d8f4cb50e9e3b53213301c6bf7
ronnyjr5644 <raunakraj20092000@gmail.com>
2023-01-29 01:23:44 +0530

    use of custom filters

---
+++ app.js
@@ -0,0 +1,48 @@
+(function () {
+'use strict';
+
+angular.module('MsgApp', [])
+.controller('MsgController', MsgController)
+.filter('loves',lovesFilter)
+.filter('truth',truthfilter);
+
+MsgController.$inject = ['$scope','$filter','lovesFilter'];
+function MsgController($scope,$filter,lovesFilter) {
+  $scope.name = "Yaakov";
+  $scope.stateOfBeing = "hungry";
+  $scope.cookieCost=.45;
+
+  $scope.sayMessage = function () {
+    var msg="Yaakov likes to eat healthy snacks at night!"
+    var output=$filter('uppercase')(msg);
+    return output;
+  };
+  $scope.sayloveMessage = function () {
+    var msg="Yaakov likes to eat healthy snacks at night!"
+    msg=lovesFilter(msg);
+    // var output=$filter('uppercase')(msg);
+    
+    return msg;
+  };
+
+  $scope.feedYaakov = function () {
+    $scope.stateOfBeing = "fed";
+  };
+}
+function lovesFilter(){
+  return function(input){
+    input=input || "";
+    input=input.replace("likes","loves");
+    return input;
+  };
+}
+function truthfilter(){
+  return function(input,target,replace){
+    input=input||"";
+    input=input.replace(target,replace);
+    return input;
+
+}
+}
+
+})();
